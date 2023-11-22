db3f6f46eeb3a370a14a8ea4be4380c35166de08
Matic Mermolja <mermoljam@gmail.com>
2023-11-22 08:26:41 +0100

    RL

--- api/middlewares/rateLimit.js
+++ api/middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const setRateLimit = require("express-rate-limit");
 
 const rateLimitMiddleware = setRateLimit({
   windowMs: 1000,
-  max: 10,
+  max: 10000,
   message: "You have exceeded your 10 requests per second limit.",
   headers: true,
 });
